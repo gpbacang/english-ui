552b0d165e9f68cda9d1d7949145d7dcd9c28a6f
Geraldine <geraldine@g-angle.co.jp>
2017-04-07 14:45:54 +0800

    Add animation to unit list

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,26 @@
 $(document).ready(function() {
-  showTitle();
+  // showTitle();
+  // unitList();
+  setTimeout(function() {
+    unitList();
+  }, 5000);
 });
 
 function showTitle() {
   var appTitle = $('#title-page').addClass('show');
   appTitle.find('img').addClass('show');
+}
+
+function unitList() {
+  var units = $('#unit-list > div:last-child > div');
+  var index = 0;
+
+  var delay = setInterval(function() {
+    if (index <= units.length) {
+      $(units[index]).addClass('show');
+      index += 1;
+    } else {
+      clearInterval(delay);
+    }
+  }, 100);
 }
